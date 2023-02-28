f42afa41c2df03dc371fbd16eea01c21e7fd6afc
Roxanapstoica <114799597+Roxanapstoica@users.noreply.github.com>
2023-02-28 19:57:54 +0200

    with_pandas

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import pandas
 from tkinter import *
-import csv
 import random
 
 
 def generate_random_word():
-    with open("data/french_words.csv", "r") as data_file:
-        data_file_content = csv.reader(data_file)
-        text = random.choice(list(data_file_content))
-        if french_word != '':
-            canvas.itemconfigure(french_word, text=f"{text[0]}")
-        return text
+    word = list_data[random.randint(0, len(list_data)-1)]['French']
+    canvas.itemconfig(french_word, text=f"{word}")
+    print(word)
+    return word
 
+
+data = pandas.read_csv("data/french_words.csv")
+list_data = data.to_dict(orient="records")
+first_french_word = list_data[random.randint(0, len(list_data)-1)]['French']
+
+print(list_data)
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
@@ -19,15 +23,11 @@ window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 icon = PhotoImage("images/France-Flag-icon.ico")
 window.iconbitmap(icon)
 
-with open("data/french_words.csv", "r") as data_file:
-    content = csv.reader(data_file)
-    french_word_text = random.choice(list(content))
-
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 my_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=my_image)
 french = canvas.create_text(400, 150, text="French", font=("calibri", 40, "italic"))
-french_word = canvas.create_text(400, 263, text=f"{french_word_text[0]}", font=("calibri", 60, "bold"))
+french_word = canvas.create_text(400, 263, text=f"{first_french_word}", font=("calibri", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 button_image_wrong = PhotoImage(file="images/wrong.png")
